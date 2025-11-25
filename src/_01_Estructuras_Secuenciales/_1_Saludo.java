db7ba28cfd573f52477a9b586f712dc5c402fce3
mfl <re-imagine.studio@proton.me>
2025-11-25 17:59:36 +0100

    First Commit

---
+++ src/_01_Estructuras_Secuenciales/_1_Saludo.java
@@ -0,0 +1,13 @@
+package _01_Estructuras_Secuenciales;
+
+public class _1_Saludo {
+    public static void main(String[] args) {
+        // Imprimir "Hola Mundo"
+        System.out.println("Hola Mundo");
+
+        // Tambien se puede imprimir desde una variable -String-
+        String saludo = "Hola Mundo 2!";
+
+        System.out.println(saludo);
+    }
+}
