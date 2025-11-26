3aa6384b980ce0577b2413b05eae8e4685295d2f
mfl <re-imagine.studio@proton.me>
2025-11-26 17:51:51 +0100

    Estructuras Secuenciales 2 & Operadores Basicos (update)

--- src/_02_Operadores/_01_Operadores_Basicos.java
+++ src/_02_Operadores/_01_Operadores_Basicos.java
@@ -4,8 +4,57 @@ public class _01_Operadores_Basicos {
     public static void main(String[] args) {
         // Operadores de Asignación
 
+        // =
+        // Asigna un valor a una variable, forma más simple de asignación.
+        int numEjemploAsignacion;
+        numEjemploAsignacion = 7;
+        System.out.println("· Comprobación de Operadores de Asignación: " + numEjemploAsignacion);
+
         // Operadores Aritméticos
+        int numero1, numero2;
+        // +
+        // Suma 2 valores
+        numero1 = 2;
+        numero2 = 4;
+        System.out.println("· Comprobación del Operador Aritmético [+]: " + (numero1 + numero2));
+
+        // -
+        // Resta 2 valores
+        numero2 = 6;
+        System.out.println("· Comprobación del Operador Aritmético [-]: " + (numero2 - numero1));
+
+        // *
+
+        // /
+
+        // %
+
+        // Operadores de Asignación Compuestos
+
+        // +=
+
+        // -=
+
+        // *=
+
+        // /=
+
+        // %=
 
         // Operadores Lógicos
+
+        // >
+
+        // <
+
+        // >=
+
+        // <=
+
+        // ||
+
+        // &&
+
+        // !
     }
 }
--- src/_03_Estructuras_Secuenciales_2/Estructuras_Secuenciales_II.java
+++ src/_03_Estructuras_Secuenciales_2/Estructuras_Secuenciales_II.java
@@ -1,9 +1,27 @@
 package _03_Estructuras_Secuenciales_2;
 
+import java.util.Scanner;
+
 public class Estructuras_Secuenciales_II {
     public static void main(String[] args) {
         /*
         Dados 2 números, solicitados al usuario, calcular y mostrar su suma, resta, división y multiplicación.
          */
+
+        int numero1, numero2;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Indica el primer número: ");
+        numero1 = input.nextInt();
+
+        System.out.println("Indica el segundo número: ");
+        numero2 = input.nextInt();
+
+        System.out.printf("""
+                La suma de ambos es %d.
+                La resta de ambos es %d.
+                La multiplicación de ambos es %d.
+                La división de ambos es %.2f.
+                """, (numero1 + numero2), (numero1 - numero2), (numero1 * numero2), ((float) numero1 / (float) numero2));
     }
 }
