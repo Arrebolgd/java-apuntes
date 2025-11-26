5e70af7373545d57a2dfc70e4463e1e29c21e622
mfl <re-imagine.studio@proton.me>
2025-11-26 18:59:29 +0100

    actualizacion Operadores: aritmeticos y aritmeticos compuestos

--- src/_02_Operadores/_01_Operadores_Basicos.java
+++ src/_02_Operadores/_01_Operadores_Basicos.java
@@ -11,6 +11,8 @@ public class _01_Operadores_Basicos {
         System.out.println("· Comprobación de Operadores de Asignación: " + numEjemploAsignacion);
 
         // Operadores Aritméticos
+        // Encargados de realizar operaciones aritméticas básicas.
+
         int numero1, numero2;
         // +
         // Suma 2 valores
@@ -20,26 +22,49 @@ public class _01_Operadores_Basicos {
 
         // -
         // Resta 2 valores
+        // Reasignamos la variable "numero2"
         numero2 = 6;
         System.out.println("· Comprobación del Operador Aritmético [-]: " + (numero2 - numero1));
 
         // *
+        // Multiplica 2 valores
+        System.out.println("· Comprobación del Operador Aritmético [*]: " + (numero1 * numero2));
 
         // /
+        // Divide 2 valores
+        float numeroDecimal1 = 7f, numeroDecimal2 = 3f;
+        System.out.println("· Comprobación del Operador Aritmético [-]: " + (numeroDecimal1 / numeroDecimal2));
 
         // %
+        // Divide 2 valores y devuelve como resultado de la operación el resto de la división
+        System.out.println("· Comprobación del Operador Aritmético [-]: " + (numeroDecimal1 % numeroDecimal2));
 
         // Operadores de Asignación Compuestos
+        // Son una mezcla de "Operación Aritmética" y "Asignación"
+        numero1 = 10;
+        numero2 = 5;
 
         // +=
+        // Suma y asigna
+        numero1 += numero2;
+        System.out.println("· Comprobación de Asignación Compuesta [+=]: " + numero1);
 
         // -=
+        // Resta y asigna
+        numero2 = 2;
+        numero1 -= numero2;
+        System.out.println("· Comprobación de Asignación Compuesta [-=]: " + numero1);
 
         // *=
+        // Multiplica y asigna
+        numero1 *= numero2;
+        System.out.println("· Comprobación de Asignación Compuesta [*=]: " + numero1);
 
         // /=
+        // Divide y asigna
 
         // %=
+        // Devuelve el modulo (resto de la división) y asigna
 
         // Operadores Lógicos
 
